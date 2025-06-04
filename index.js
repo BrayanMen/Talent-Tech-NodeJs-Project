4fc1b851e2ff45a2f8b27b21c499aca97076e88d
BrayanMen <brayanjmr880@gmail.com>
2025-06-04 00:58:11 -0300

    run

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const API_URL = 'https://fakestoreapi.com';
 const getProductos = async () => {
     try {
         const res = await fetch(`${API_URL}/products`);
+        if (!res.ok) throw new Error('Error al obtener productos');
         const data = await res.json();
 
         console.log('Productos: ');
@@ -17,6 +18,7 @@ const getProductos = async () => {
 
 const getProductoPorId = async id => {
     try {
+        if (!id) throw new Error('Debes pasar por parametro un ID');
         const res = await fetch(`${API_URL}/products/${id}`);
         if (!res.ok) throw new Error(`El producto ${id} no fue encontrado.`);
         const data = await res.json();
@@ -33,20 +35,24 @@ const getProductoPorId = async id => {
     }
 };
 
-const crearProducto = async (title, price, category)=>{
+const crearProducto = async (title, price, category) => {
     try {
+        if (!title || !price || !category) throw new Error('Faltan datos');
+        const parsedPrice = parseFloat(price);
+        if (isNaN(parsedPrice)) throw new Error('El precio debe ser un número');
+
         const nuevoProducto = {
             title: title,
-            price: parseFloat(price),
-            category:category,
-            description: `Producto ${title} en categoria ${category}`
-        }
-        const res = await fetch(`${API_URL}/products`,{
+            price: parsedPrice,
+            category: category,
+            description: `Producto ${title} en categoria ${category}`,
+        };
+        const res = await fetch(`${API_URL}/products`, {
             method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
+            headers: {
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(nuevoProducto)
+            body: JSON.stringify(nuevoProducto),
         });
 
         const resultado = await res.json();
@@ -58,9 +64,54 @@ const crearProducto = async (title, price, category)=>{
         - ${resultado.title} 
         - ${resultado.category}
         - ${resultado.price}`);
-        
     } catch (err) {
         console.error('Error: ', err.message);
     }
-}
+};
 
+const eliminarProducto = async id => {
+    try {
+        if (!id) throw new Error('Debes pasar por parametro un ID');
+
+        const res = await fetch(`${API_URL}/products/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (res.ok) {
+            console.log('Producto eliminado');
+        } else {
+            throw new Error('No se encuentra el producto');
+        }
+    } catch (err) {
+        console.error('Error: ', err.message);
+    }
+};
+
+const [, , method, , ...args] = process.argv;
+
+const run = async () => {
+    try {
+        switch (method) {
+            case 'GET':
+                args.length === 0 ? getProductos() : getProductoPorId(args[0]);
+                break;
+            case 'POST':
+                const [title, price, category] = args;
+                crearProducto(title, price, category);
+                break;
+            case 'DELETE':
+                eliminarProducto(args[0]);
+                break;
+            default:
+                console.log(`
+                    Método inválido. Usa:
+                    \n - npm run start GET
+                    \n - npm run start GET <id>
+                    \n - npm run start POST <title> <price> <category>
+                    \n - npm run start DELETE <id>
+                    `);
+        }
+    } catch (err) {
+        console.error('Error: ', err.message);
+    }
+};
